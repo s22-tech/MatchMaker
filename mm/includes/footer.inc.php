090cb46a84fb40bf491f6f117e765ffbec35e3e9
s22_tech <lc@s22.us>
2021-06-15 14:56:03 -0700

    Cleanup merge

--- mm/includes/footer.inc.php
+++ mm/includes/footer.inc.php
@@ -13,20 +13,6 @@
 		<script src="<?php echo $config['rewrite_base'] ?>/includes/js/fslightbox.js"></script>
 		<script src="<?php echo $config['rewrite_base'] ?>/includes/js/bootstrap.min.js"></script>
 		<script src="<?php echo $config['rewrite_base'] ?>/includes/js/functions.js"></script>
-<<<<<<< HEAD
-=======
-
-		<script>
-			$( document ).on( "pagecreate", function() {
-				$( ".photopopup" ).on({
-					popupbeforeposition: function() {
-						var maxHeight = $( window ).height() - 60 + "px";
-						$( ".photopopup img" ).css( "max-height", maxHeight );
-					}
-				});
-			});
-
->>>>>>> 233e7a72dc517c78f238f667e87d765c4b60e8c3
 
 		<script>
 		  // Choose from 3 different button styles.
@@ -43,7 +29,6 @@
 			});
 
 
-<<<<<<< HEAD
 			$(document).on("pagecreate", function() {
 				$(".photopopup").on({
 					popupbeforeposition: function() {
@@ -53,8 +38,6 @@
 				});
 			});
 		</script>
-=======
->>>>>>> 233e7a72dc517c78f238f667e87d765c4b60e8c3
 
   </body>
 </html>
